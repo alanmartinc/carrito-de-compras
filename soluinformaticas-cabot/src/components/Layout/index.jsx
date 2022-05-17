58978c2331f56107ec886f716b1ce6af1aeec42f
alanmartinc <alancabot.ac@gmail.com>
2022-05-17 18:10:19 -0300

    Se agrega cambios

--- soluinformaticas-cabot/src/components/Layout/index.jsx
+++ soluinformaticas-cabot/src/components/Layout/index.jsx
@@ -3,36 +3,36 @@ import NavBar from "../NavBar";
 import DropdownMenu from "../NavBar/DropdownMenu";
 import Footer from "../Footer";
 
-export default function Layout({children}) {
-    const [isOpen, setIsOpen] = useState(false);
+export default function Layout({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
-        const hideMenu = () => {
-            if(window.innerWidth > 768 && isOpen) {
-                setIsOpen(false);
-            }
-        };
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
 
-        window.addEventListener("resize", hideMenu);
+    window.addEventListener("resize", hideMenu);
 
-        return() => {
-            window.removeEventListener("resize", hideMenu);
-        };
-    });
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
 
-    const toggleOpen = () => {
-        setIsOpen(!isOpen);
-    }
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <Fragment>
-            <NavBar handleOpenClose={toggleOpen} />
+  return (
+    <Fragment>
+      <NavBar handleOpenClose={toggleOpen} />
 
-            {isOpen && <DropdownMenu handleOpenCloseNew={toggleOpen} />}
+      {isOpen && <DropdownMenu handleOpenCloseNew={toggleOpen} />}
 
-            {children}
-            
-            <Footer />
-        </Fragment>
-    );
+      {children}
+
+      <Footer />
+    </Fragment>
+  );
 }
--- soluinformaticas-cabot/src/views/Cart/index.jsx
+++ soluinformaticas-cabot/src/views/Cart/index.jsx
@@ -19,7 +19,36 @@ export default function Cart() {
   return (
     <Fragment>
       {cart.length === 0 && !id && !creatingOrder && (
-        <h1>No hay productos en el carrito</h1>
+        <div className="max-w-2xl mx-auto pt-16 pb-6 px-4 sm:pt-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h1 className="tracking-tight font-extrabold text-gray-900 text-2xl md:text-4xl mb-6">
+              No hay productos en el carrito
+            </h1>
+          </div>
+
+          <div className="grid grid-cols-1 gap-y-10 gap-x-6 xl:gap-x-8 mb-6">
+            <div key="id">
+              <div className="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img
+                  src="assets/accessDenied.png"
+                  alt="Error 404"
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center">
+            <div className="rounded-md shadow">
+              <a
+                href={`products`}
+                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-800 md:py-4 md:text-lg md:px-10"
+              >
+                Agregar
+              </a>
+            </div>
+          </div>
+        </div>
       )}
       {!creatingOrder && !id && cart.length > 0 && (
         <div className="max-w-2xl mx-auto pt-16 pb-6 px-4 sm:pt-24 sm:px-6 lg:px-8">
@@ -87,9 +116,40 @@ export default function Cart() {
         </div>
       )}
 
-      {creatingOrder && !id && <h1>Loading....</h1>}
+      {creatingOrder && !id && <Spinner />}
 
-      {!creatingOrder && id && <h1>Orden {id} comprado exitosamente!</h1>}
+      {!creatingOrder && id && (
+        <div className="max-w-2xl mx-auto pt-16 pb-6 px-4 sm:pt-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h1 className="tracking-tight font-extrabold text-gray-900 text-2xl md:text-4xl mb-6">
+              Orden {id} comprado exitosamente!
+            </h1>
+          </div>
+
+          <div className="grid grid-cols-1 gap-y-10 gap-x-6 xl:gap-x-8 mb-6">
+            <div key="id">
+              <div className="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img
+                  src="assets/orderConfirmed.png  "
+                  alt="Error 404"
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center">
+            <div className="rounded-md shadow">
+              <a
+                href={`products`}
+                className="w-full h-14 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-800 md:py-4 md:text-lg md:px-10"
+              >
+                Volver
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
     </Fragment>
   );
 }
